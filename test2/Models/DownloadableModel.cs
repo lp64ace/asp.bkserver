ee0cebc724359b12dcde99ea97d695e74e55444f
jim codeman <akomikron@gmail.com>
2022-08-01 00:58:31 +0300

    browse page.

---
+++ test2/Models/DownloadableModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test2.Models
+{
+	public class DownloadableModel
+	{
+		public int Id { get; set; }
+		public string ContainingFolder { get; set; }
+		public string Name { get; set; }
+		public DateTime DateModified { get; set; }
+		public long Size { get; set; }
+		
+		public enum InternalTypeEnum
+		{
+			FILE,
+			FOLDER,
+		};
+
+		public InternalTypeEnum InternalType { get; set; }
+	}
+}
